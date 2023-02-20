6ca89a92997235eee4e977357376d373755b1c22
dishanubari <dishanubaripramudia07@gmail.com>
2023-02-20 14:47:10 +0700

    property

--- produk.php
+++ produk.php
@@ -7,3 +7,18 @@ class Produk {
             $harga = 0;
 }
 
+//$produk1 = new Produk();
+//$produk1->judul = "Doraemon";
+//var_dump($produk1);
+
+//$produk2 = new Produk();
+//$produk2->judul = "PES";
+//var_dump($produk2->judul);
+
+$produk3 = new Produk();
+$produk3->judul = "Hujan";
+$produk3->penulis = "Tere Liye";
+$produk3->penerbit = "PT Bentang Aksara";
+$produk3->harga = 50000;
+
+echo "Novel : $produk3->penulis, $produk3->penerbit";

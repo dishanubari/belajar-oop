1f58401c5263dca549f627c1a4d063cf30a8fbed
dishanubari <dishanubaripramudia07@gmail.com>
2023-02-20 15:02:56 +0700

    latihan method

--- produk.php
+++ produk.php
@@ -5,6 +5,10 @@ class Produk {
             $penulis = "penulis",
             $penerbit = "penerbit",
             $harga = 0;
+
+    public function getLabel() {
+        return "$this->judul, $this->penulis";
+    }
 }
 
 //$produk1 = new Produk();
@@ -21,4 +25,12 @@ $produk3->penulis = "Tere Liye";
 $produk3->penerbit = "PT Bentang Aksara";
 $produk3->harga = 50000;
 
-echo "Novel : $produk3->penulis, $produk3->penerbit";
+$produk4 = new Produk();
+$produk4->judul = "Uncharted";
+$produk4->penulis = "Neil Druckman";
+$produk4->penerbit = "Sony Computer";
+$produk4->harga = 250000;
+
+echo "Novel : " . $produk3->getLabel();
+echo "<br>";
+echo "Game : " . $produk4->getLabel();

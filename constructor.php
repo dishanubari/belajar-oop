edf5646ca852f1cda83d934926fdb2a5d43e0f32
dishanubari <dishanubaripramudia07@gmail.com>
2023-02-21 16:08:46 +0700

    memberi nilai default

--- constructor.php
+++ constructor.php
@@ -1,12 +1,14 @@
 <?php
 
-class Produk {
-    public $judul = "judul",
-            $penulis = "penulis",
-            $penerbit = "penerbit",
-            $harga = 0;
+use Produk as GlobalProduk;
 
-    public function __construct( $judul, $penulis, $penerbit, $harga ){
+class Produk {
+    public $judul,
+            $penulis,
+            $penerbit,
+            $harga;
+
+    public function __construct( $judul ="judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0 ){
         $this->judul = $judul;
         $this->penulis = $penulis;
         $this->penerbit = $penerbit;
@@ -20,8 +22,10 @@ class Produk {
 
 $produk1 = new Produk("Hujan", "Tere Liye", "PT Bentang Aksara", 50000);
 $produk2 = new Produk("Uncharted", "Neil Druckman", "Sony Computer", 250000);
+$produk3 = new Produk("Dragonball");
 
-
-echo "Novel : " . $produk1->getLabel();
+echo "Novel : " . $produk1->getLabel(); 
 echo "<br>";
 echo "Game : " . $produk2->getLabel();
+echo "<br>";
+var_dump($produk3);

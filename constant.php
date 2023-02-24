944891479b3cdb0c62e08ff17de6a7c23ab83196
dishanubari <dishanubaripramudia07@gmail.com>
2023-02-24 08:07:11 +0700

    latihan constant

---
+++ constant.php
@@ -0,0 +1,19 @@
+ <?php
+
+//  define('NAMA', 'Dishanubari Pramudia');
+//  echo NAMA;
+
+
+//  const UMUR = 32;
+//  echo UMUR;
+
+
+class Coba {
+    const NAMA = 'Dishanubari';
+}
+
+echo Coba::NAMA;
+
+
+ ?>
+
